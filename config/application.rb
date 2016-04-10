d14eac6f6ea5dc858bec8e90644c96086497b3c6
Carlos Coto <cmcoto@gmail.com>
2016-04-10 14:14:49 -0600

    Modify Config

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,8 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+#Bundler.require(*Rails.groups)
+Bundler.require(:default, Rails.env)
 
 # config/application.rb
 require 'apartment/elevators/subdomain' # or 'domain' or 'generic'
