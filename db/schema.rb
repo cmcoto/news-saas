671ca90c79cebf47b05f596fd688f3452a8a3897
Carlos Coto <cmcoto@gmail.com>
2016-04-09 21:31:57 -0600

    Association between User and Files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408005842) do
+ActiveRecord::Schema.define(version: 20160410030638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160408005842) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "projects", force: :cascade do |t|
